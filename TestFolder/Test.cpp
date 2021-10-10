4582f7a1cf83c4cb5e596d2a4b35178a90cb7475
sahilshembekar <sahil.shembekar@gmail.com>
2021-10-10 20:35:04 +0900

    Third Commit

---
+++ TestFolder/Test.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int a {}, b{}, c{};
+
+int main() {
+    cout << "Enter 3 numbers with space: ";
+    cin >> a >> b >> c;
+    
+    cout << "The sum is: " << (a+b+c) << endl;
+    //cout << (a+b+c) <<endl;
+    return 0;
+}
