35255614cb34926161af410ed0936f1f10bd6e26
sahilshembekar <sahil.shembekar@gmail.com>
2021-10-31 14:53:52 +0900

    MergeSort

---
+++ MergeSort/main.cpp
@@ -0,0 +1,95 @@
+#include<stdlib.h> 
+#include<stdio.h> 
+  
+// Merges two subarrays of arr[]. 
+// First subarray is arr[l..m] 
+// Second subarray is arr[m+1..r] 
+void merge(int arr[], int l, int m, int r) 
+{ 
+    //Your code here
+    int sizeOfArray1 = m-l+1;
+    int sizeOfArray2 = r-m;
+
+    int a[sizeOfArray1];
+    int b[sizeOfArray2];
+
+    for (int i =0; i< sizeOfArray1; i++) {
+        a[i] = arr[l+i];
+    }
+
+    for (int i =0; i<sizeOfArray2; i++) {
+        b[i] = arr[m+1+i];
+    }
+
+    int i =0;
+    int j =0;
+    int k =l;
+
+    while(i<sizeOfArray1 && j<sizeOfArray2) {
+        if (a[i]< b[j]) {
+            arr[k] = a[i];
+            k++;
+            i++;
+        }
+        else  {
+            arr[k]=b[j];
+            k++;
+            j++;
+        }
+    }
+    while (i<sizeOfArray1) {
+        arr[k] = a[i];
+        k++;
+        i++;
+    }
+    while (j<sizeOfArray2) {
+        arr[k] = b[j];
+        k++;
+        j++;
+    }
+
+
+
+
+
+
+} 
+  
+/* l is for left index and r is right index of the 
+   sub-array of arr to be sorted */
+void mergeSort(int arr[], int l, int r) 
+{ 
+    //Your code here
+    if (l<r) {
+        int mid = l + (r-l)/2;
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid+1, r);
+        merge(arr, l, mid, r);
+    }
+} 
+  
+/* UTILITY FUNCTIONS */
+/* Function to print an array */
+void printArray(int A[], int size) 
+{ 
+    int i; 
+    for (i=0; i < size; i++) 
+        printf("%d ", A[i]); 
+    printf("\n"); 
+} 
+  
+/* Driver program to test above functions */
+int main() 
+{ 
+    int arr[] = {12, 11, 13, 5, 6, 7}; 
+    int arr_size = sizeof(arr)/sizeof(arr[0]); 
+  
+    printf("Given array is \n"); 
+    printArray(arr, arr_size); 
+  
+    mergeSort(arr, 0, arr_size - 1); 
+  
+    printf("\nSorted array is \n"); 
+    printArray(arr, arr_size); 
+    return 0; 
+}
